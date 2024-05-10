d9861211b77bf74ab580e97b6843113d0440e91a
Myo Zaw Lin <mzlmyo@gmail.com>
2024-05-09 22:02:00 -0500

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Myo Zaw Lin on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
